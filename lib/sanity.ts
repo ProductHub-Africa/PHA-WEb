a6841c98e612687a28aa2bfc16a6557f14f15eb5
Product Hub Africa <producthubafrica@gmail.com>
2025-12-30 01:23:19 +0100

    feat: Remove Sanity integration
    This commit removes all Sanity.io related code and configurations. The project will now use internal mock data for blog content, simplifying dependencies and build processes.

--- lib/sanity.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Sanity Integration Removed.
- * This file is kept as a minimal export to prevent import errors in other files.
- */
-export const sanityClient = null;
-export default null;
--- sanity-setup/README.md
+++
@@ -1,2 +0,0 @@
-# Sanity Integration Removed
-This directory is no longer in use as the project has switched back to internal mock data for blog content.
